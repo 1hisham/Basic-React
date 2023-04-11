e39da04b18515957efa99044c90a67dd7a7dabc0
1hisham <hisham1ahmmed@gmail.com>
2023-04-11 22:31:24 +0530

    added recipe app

--- recipeapp/src/App.js
+++ recipeapp/src/App.js
@@ -1,12 +1,18 @@
 
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Category from './components/Category';
 import Pages from './pages/Pages';
 
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+    <Category />
       <Pages />
+      </BrowserRouter>
+
      <h1>hello</h1>
     </div>
   );
--- recipeapp/src/pages/Home.jsx
+++ recipeapp/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Popular from "../components/Popular";
 
 import React from 'react'
 
+
 function Home() {
   return (
     <div>
